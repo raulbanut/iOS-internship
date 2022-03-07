ff21c2affaca66e696aaf554418fd295655f4270
Raul Banut <banut.raul@yahoo.com>
2022-03-07 10:43:11 +0200

    Add exercises with protocols

--- W1_1_6.playground/Contents.swift
+++ W1_1_6.playground/Contents.swift
@@ -1,3 +1,73 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Protocols
+ A protocol defines a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
+ */
+
+protocol Employee {
+    var name: String { get set }
+    var jobTitle: String { get set }
+    func doWork()
+}
+
+struct Executive: Employee {
+    var name = "Steve Jobs"
+    var jobTitle = "CEO"
+    
+    func doWork() {
+        print("I'm strategizing!")
+    }
+}
+
+struct Manager: Employee {
+    var name = "Maurice Moss"
+    var jobTitle = "Head of IT"
+    
+    func doWork() {
+        print("I'm turning it off and on again!")
+    }
+}
+
+let staff: [Employee] = [Executive(), Manager()]
+
+for person in staff {
+    person.doWork()
+}
+
+
+/*
+ Protocols as Types
+ */
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+                        .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
