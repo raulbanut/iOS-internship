e829c950c20fabcf6f494aad30d34ed542b785d7
Banut Raul <banut.raul@yahoo.com>
2022-03-10 17:00:58 +0200

    Add new project in SwiftUI

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,37 +14,119 @@ struct ContentView: View {
     @State private var name = ""
     @State private var selectedStudent = "Ron"
     
+    @State private var amount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    var currencyType: FloatingPointFormatStyle<Double>.Currency {
+        return .currency(code: Locale.current.currencyCode ?? "RON")
+    }
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = amount / 100 * tipSelection
+        let grandValue = amount + tipValue
+        let amountPerPerson = grandValue / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = amount / 100 * tipSelection
+        let grandValue = amount + tipValue
+        
+        return grandValue
+    }
+    
     var body: some View {
         NavigationView{
             Form {
                 Section {
-                    Picker("Select your student: ", selection: $selectedStudent) {
-                        ForEach(students, id: \.self) {
-                            Text($0)
+                    TextField("Amount", value: $amount, format: currencyType)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
                         }
                     }
                 }
                 
                 Section {
-                    Text("Hello, world!")
-                }
-                
-                Group {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        //                        ForEach(tipPercentages, id: \.self) {
+                        //                            Text($0, format: .percent)
+                        //                        }
+                        ForEach(0..<101) {
+                            Text("\($0)%")
+                        }
+                    }
+                    //                    .pickerStyle(.segmented)
+                    //                    .pickerStyle(.wheel)
+                } header: {
+                    Text("How much tip do you want to leave?")
                 }
                 
                 Section {
-                    TextField("Enter you name", text: $name)
-                    Text("Hello \(name)")
+                    Text(totalPerPerson, format: currencyType)
+                } header: {
+                    Text("Amount per person")
                 }
                 
                 Section {
-                    Button("Tap Count: \(tapCount)") {
-                        tapCount += 1
+                    Text(totalAmount, format: currencyType)
+                } header: {
+                    Text("The total amount with tip")
+                }
+                
+                //                Section {
+                //                    Picker("Select your student: ", selection: $selectedStudent) {
+                //                        ForEach(students, id: \.self) {
+                //                            Text($0)
+                //                        }
+                //                    }
+                //                }
+                //
+                //                Section {
+                //                    Text("Hello, world!")
+                //                }
+                //
+                //                Group {
+                //                    Text("Hello, world!")
+                //                    Text("Hello, world!")
+                //                }
+                //
+                //                Section {
+                //                    TextField("Enter you name", text: $name)
+                //                    Text("Hello \(name)")
+                //                }
+                //
+                //                Section {
+                //                    Button("Tap Count: \(tapCount)") {
+                //                        tapCount += 1
+                //                    }
+                //                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
-            }.navigationTitle("SwiftUI")
+            }
+            
         }
     }
 }
